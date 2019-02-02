c40118c4dda2f11b5ae0f3df90baa3148cf201f8
inari111 <takuyakoori+github@gmail.com>
2019-02-02 17:49:19 +0900

    move NewHandler

--- handler/api/article.go
+++ handler/api/article.go
@@ -2,12 +2,9 @@ package api
 
 import (
 	"context"
-	"net/http"
 
 	articleApp "github.com/inari111/layered-architecture-study/application/article"
-	"github.com/twitchtv/twirp"
 
-	"github.com/inari111/layered-architecture-study/handler"
 	"github.com/inari111/layered-architecture-study/handler/rpc"
 )
 
@@ -35,35 +32,4 @@ func (s *articleService) Create(ctx context.Context, req *pb.ArticleServiceCreat
 			Body:  entity.Body,
 		},
 	}, nil
-}
-
-type server struct {
-	pathPrefix string
-	twServer   pb.TwirpServer
-}
-
-func NewHandler(
-	articleService pb.ArticleService,
-) http.Handler {
-	mux := http.NewServeMux()
-
-	hooks := twirp.ChainHooks(
-		handler.UserAuthHooks(),
-		handler.LoggingErrorHooks(),
-	)
-
-	servers := []server{
-		{
-			pathPrefix: pb.ArticleServicePathPrefix,
-			twServer: pb.NewArticleServiceServer(
-				articleService,
-				hooks,
-			),
-		},
-	}
-
-	for _, s := range servers {
-		mux.Handle(s.pathPrefix, handler.InjectAppEngineContext(s.twServer))
-	}
-	return mux
 }
